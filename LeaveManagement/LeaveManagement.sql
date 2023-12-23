a83f50dd7d4940c10e0ebbb6fe1dd86b2468bd10
RevanthKongari <146952582+RevanthKongari@users.noreply.github.com>
2023-12-23 15:34:20 +0530

    Add files via upload
    The main functionality of a project is to  granting and declining leaves requested by a employee as per company criteria such as fixed number of leaves(casual, annual, medical)
    per year.

---
+++ LeaveManagement/LeaveManagement.sql
@@ -0,0 +1,25 @@
+
+
+create database TBS;
+use TBS;
+
+create table registration(empno varchar(45) primary key,empname varchar(100),category varchar(100),sal int8);
+
+create table Leaveinfo (empnum varchar(20),casual int8,annual int8,medical int8, annum int8);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
